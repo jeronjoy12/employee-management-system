a77f0b268c177d2709a377c5f949b9917ea01a0c
jeronjoy12 <jeronjoy@retailcloud.com>
2026-06-09 14:52:46 +0530

    chore: update title on develop

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  title = 'RetailCloud EMS';
+  title = 'Hi,Dashboard';
 
   employeeCount = 100;
   employeeName = '';
